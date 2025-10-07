8c5b24f49deeb7a9f7ff844d22832bfd319e361a
iamphelipe <phelipeguimaraes2023@gmail.com>
2025-10-07 15:14:24 -0300

    feature Relaotio already

---
+++ app/src/main/java/com/example/urbanize/ui/screen/RelatorioAnimalScreen.kt
@@ -0,0 +1,24 @@
+package com.example.urbanize.ui.screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+    fun RelatorioAnimalScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(text = "Tela de Relatório de Animal", style = MaterialTheme.typography.headlineSmall)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { navController.popBackStack() }) {
+            Text("Voltar")
+        }
+    }
+}
